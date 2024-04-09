5310eb2f3be7ce4d029ee6bd70b1479df5edb737
jinsu868 <mjinsu098@gmail.com>
2024-04-09 20:51:45 +0900

    feat: s3 파일 업로드, 삭제 기능 구현 (#8)

---
+++ backend/src/main/java/momo/app/image/S3Service.java
@@ -0,0 +1,57 @@
+package momo.app.image;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class S3Service {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    public String upload(MultipartFile file) {
+        String imageUrl = "";
+        String fileName = createFileName(file.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+
+        try (InputStream inputStream = file.getInputStream()) {
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            imageUrl = amazonS3.getUrl(bucket, fileName).toString();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("failed to upload");
+        }
+        return imageUrl;
+    }
+
+    public void deleteFile(String imageUrl) {
+        DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(
+                bucket , parseKeyUrl(imageUrl));
+        amazonS3.deleteObject(deleteObjectRequest);
+    }
+
+    private String parseKeyUrl(String imageUrl) {
+        return imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+    }
+
+    private String createFileName(String fileName) {
+        FileExtension extension = FileExtension.from(fileName);
+        return UUID.randomUUID().toString().concat(extension.getExtension());
+    }
+}
