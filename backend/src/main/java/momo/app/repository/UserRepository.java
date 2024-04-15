5b76c41e7a99d4c9a6cb9a325436cef8cb4b81f1
eogus0512 <eogus0512@gmail.com>
2024-04-15 23:54:33 +0900

    feat: User 관련 MVC 생성

---
+++ backend/src/main/java/momo/app/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package momo.app.repository;
+
+import momo.app.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findBySocialId(String socialId);
+}
