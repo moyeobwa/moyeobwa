276ba98b04a5e75a095433fda6138c177610bfae
jinsu868 <mjinsu098@gmail.com>
2024-04-19 01:37:55 +0900

    feat: HashTag 도메인 작성 #15

---
+++ backend/src/main/java/momo/app/gathering/domain/GatheringTag.java
@@ -0,0 +1,37 @@
+package momo.app.gathering.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GatheringTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gathering_id")
+    private Gathering gathering;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @Builder
+    public GatheringTag(Gathering gathering, Tag tag) {
+        this.gathering = gathering;
+        this.tag = tag;
+    }
+}
---
+++ backend/src/main/java/momo/app/gathering/domain/Tag.java
@@ -0,0 +1,30 @@
+package momo.app.gathering.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 45)
+    private String name;
+
+    @Builder
+    public Tag(String name) {
+        this.name = name;
+    }
+
+}
