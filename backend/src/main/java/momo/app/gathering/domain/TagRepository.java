d0f370d58f7676a683b0bfc07cfe67e2fd23b859
jinsu868 <mjinsu098@gmail.com>
2024-04-19 16:32:54 +0900

    refactor: Tag 패키지 분리 #15

--- backend/src/main/java/momo/app/gathering/domain/TagRepository.java
+++
@@ -1,8 +0,0 @@
-package momo.app.gathering.domain;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-    Optional<Tag> findByName(String name);
-}
