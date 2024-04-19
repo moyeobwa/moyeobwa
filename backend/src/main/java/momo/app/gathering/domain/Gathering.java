30f277e6cfcec757027519469014ff445e89d621
jinsu868 <mjinsu098@gmail.com>
2024-04-19 16:56:14 +0900

    feat: 방장 식별 기능 추가 #15

--- backend/src/main/java/momo/app/gathering/domain/Gathering.java
+++ backend/src/main/java/momo/app/gathering/domain/Gathering.java
@@ -11,6 +11,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import momo.app.auth.dto.AuthUser;
 import momo.app.common.domain.BaseTime;
 
 @Entity
@@ -30,6 +31,7 @@ public class Gathering extends BaseTime {
 
     private String imageUrl;
 
+    private Long managerId;
 
     @OneToMany(mappedBy = "gathering")
     private List<GatheringTag> gatheringTags = new ArrayList<>();
@@ -40,16 +42,23 @@ public class Gathering extends BaseTime {
             String name,
             String description,
             String imageUrl,
-            String hashTag
+            Long managerId
     ) {
         this.name = name;
         this.description = description;
         this.imageUrl = imageUrl;
         this.category = category;
+        this.managerId = managerId;
     }
 
     public void addGatheringTag(GatheringTag gatheringTag) {
         gatheringTags.add(gatheringTag);
+    }
+
+    public void validateManager(AuthUser authUser) {
+        if (authUser.getId() != managerId) {
+            throw new IllegalStateException("Gathering can only be modified by the host.");
+        }
     }
 
 }
