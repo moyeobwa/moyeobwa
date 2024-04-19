311e34aa46e46828465bcaeef3349b3a9339dc1d
jinsu868 <mjinsu098@gmail.com>
2024-04-19 16:28:47 +0900

    feat: Gathering, Tag, GatheringTag JPA 레포 작성 #15

---
+++ backend/src/main/java/momo/app/gathering/domain/GatheringTagRepository.java
@@ -0,0 +1,6 @@
+package momo.app.gathering.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GatheringTagRepository extends JpaRepository<GatheringTag, Long> {
+}
