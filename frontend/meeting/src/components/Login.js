802c53f8eea38be1ccfdca4ae5413df2f341971a
khanz0613 <62611268+khanz0613@users.noreply.github.com>
2024-04-21 08:06:21 +0900

    feat: 로그인 URL 변경

--- frontend/meeting/src/components/Login.js
+++ frontend/meeting/src/components/Login.js
@@ -2,20 +2,16 @@ import React from 'react';
 
 class LoginScreen extends React.Component {
   handleLoginClick = () => {
-    console.log('로그인 버튼이 클릭됨')
+    console.log('로그인 버튼이 클릭됨');
   }
-  
 
   handleGoogleLogin = () => {
-    const clientId = process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID;
-    const redirectUri = process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI;
-    const scope = 'email profile';
-    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline`;
-    window.location.href = authUrl;
+    // Google OAuth 페이지로 리다이렉트
+    window.location.href = 'http://ec2-13-125-224-63.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/google';
   }
 
   render() {
-    const {onLogin} = this.props;
+    const { onLogin } = this.props;
     const buttonStyle = {
       background: `url('/google_login_button.jpg') no-repeat center center`,
       backgroundSize: '100% 100%',
@@ -30,11 +26,10 @@ class LoginScreen extends React.Component {
       paddingBottom: '50px'
     }
 
-
     return (
       <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#ffffff' }}>
         <h1 style={textStyle}>안녕하세요, OOO입니다.</h1>
-        <h2 style = {textStyle}>OO을 이용해 모임을 관리해보세요!</h2>
+        <h2 style={textStyle}>OO을 이용해 모임을 관리해보세요!</h2>
         <button style={buttonStyle} onClick={this.handleGoogleLogin}></button>
       </div>
     );
