f997dc431f8de5a2c5dd7625ccd0ad70d1e919aa
jinsu868 <mjinsu098@gmail.com>
2024-05-20 15:00:45 +0900

    feat: 채팅방 엔티티 뼈대 작성 (#56)

---
+++ backend/src/main/java/momo/app/chat/domain/chatroom/ChatRoom.java
@@ -0,0 +1,43 @@
+package momo.app.chat.domain.chatroom;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import momo.app.chat.domain.chatmessage.ChatMessage;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long managerId;
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<ChatRoomUser> chatRoomUsers = new ArrayList<>();
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+    @Builder
+    public ChatRoom(Long managerId) {
+        this.isActive = false;
+        this.managerId = managerId;
+    }
+}
