6e07f32dacaa606809981456b39b537ab156c320
jinsu868 <mjinsu098@gmail.com>
2024-05-20 15:01:59 +0900

    feat: 모임 채팅방 생성 로직 추가 (#56)

--- backend/src/main/java/momo/app/gathering/service/GatheringCommandService.java
+++ backend/src/main/java/momo/app/gathering/service/GatheringCommandService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import momo.app.auth.dto.AuthUser;
+import momo.app.chat.domain.chatroom.ChatRoom;
+import momo.app.chat.domain.chatroom.ChatRoomRepository;
 import momo.app.gathering.domain.Gathering;
 import momo.app.gathering.domain.GatheringInfo;
 import momo.app.gathering.domain.GatheringMember;
@@ -32,6 +34,7 @@ public class GatheringCommandService {
     private final S3Service s3Service;
     private final TagRepository tagRepository;
     private final GatheringTagRepository gatheringTagRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
     public Long createGathering(GatheringCreateRequest request, AuthUser authUser) {
         User user = findUser(authUser.getId());
@@ -42,9 +45,15 @@ public class GatheringCommandService {
                 request.description(),
                 uploadedImageUrl
         );
+
+        ChatRoom chatRoom = chatRoomRepository.save(ChatRoom.builder()
+                .managerId(authUser.getId())
+                .build());
+
         Gathering gathering = Gathering.builder()
                 .gatheringInfo(gatheringInfo)
                 .managerId(user.getId())
+                .chatRoomId(chatRoom.getId())
                 .build();
 
         gatheringRepository.save(gathering);
