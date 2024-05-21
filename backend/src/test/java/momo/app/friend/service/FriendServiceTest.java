fbe24280243c470ebcb920c1fe3a3b28764c2d66
eogus0512 <eogus0512@gmail.com>
2024-05-21 16:30:28 +0900

    feat: mock 라이브러리 추가

---
+++ backend/src/test/java/momo/app/friend/service/FriendServiceTest.java
@@ -0,0 +1,92 @@
+package momo.app.friend.service;
+
+import momo.app.auth.dto.AuthUser;
+import momo.app.common.error.exception.BusinessException;
+import momo.app.friend.domain.Friend;
+import momo.app.friend.domain.FriendRepository;
+import momo.app.friend.domain.FriendState;
+import momo.app.friend.exception.FriendErrorCode;
+import momo.app.user.domain.Role;
+import momo.app.user.domain.User;
+import momo.app.user.domain.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static momo.app.auth.dto.AuthUser.createAuthUser;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+
+class FriendServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private FriendRepository friendRepository;
+
+    @InjectMocks
+    private FriendService friendService;
+
+    private User user1;
+    private User user2;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+
+        user1 = new User(1L, "socialId1", "User1", "user1", "user1@example.com", "image1", "refreshToken1", Role.GUEST, "active", "desc1", "interest1", new ArrayList<>());
+        user2 = new User(2L, "socialId2", "User2", "user2", "user2@example.com", "image2", "refreshToken2", Role.GUEST, "active", "desc2", "interest2", new ArrayList<>());
+    }
+
+    @Test
+    void testRequest() {
+        AuthUser authUser = createAuthUser(user1);
+        when(userRepository.findById(user1.getId())).thenReturn(Optional.of(user1));
+        when(userRepository.findById(user2.getId())).thenReturn(Optional.of(user2));
+
+        friendService.request(2L, authUser);
+
+        verify(friendRepository, times(1)).save(any());
+        verify(userRepository, times(1)).findById(user1.getId());
+        verify(userRepository, times(1)).findById(user2.getId());
+    }
+
+    @Test
+    void testRequestExceptionOnSecondCall() {
+        AuthUser authUser = createAuthUser(user1);
+        when(userRepository.findById(user1.getId())).thenReturn(Optional.of(user1));
+        when(userRepository.findById(user2.getId())).thenReturn(Optional.of(user2));
+        when(friendRepository.findByTwoUser(any(), any())).thenThrow(new BusinessException(FriendErrorCode.FRIEND_REQUEST_ALREADY_EXISTS));
+
+        assertThrows(BusinessException.class, () -> friendService.request(2L, authUser));
+
+        verify(friendRepository, times(1)).findByTwoUser(any(), any());
+    }
+
+    @Test
+    void testAccept() {
+        AuthUser authUser = createAuthUser(user1);
+        Friend friend = Friend.builder()
+                .fromUser(user2)
+                .toUser(user1)
+                .friendState(FriendState.WAITING)
+                .build();
+        when(userRepository.findById(user2.getId())).thenReturn(Optional.of(user2));
+        when(friendRepository.findById(1L)).thenReturn(Optional.of(friend));
+        when(userRepository.findById(user1.getId())).thenReturn(Optional.of(user1));
+
+        friendService.accept(1L, authUser);
+
+        verify(friendRepository, times(1)).save(any());
+        verify(userRepository, times(1)).findById(user1.getId());
+    }
+}
