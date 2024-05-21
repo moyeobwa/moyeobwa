f0c04d36c8d498583ee243b871d84b2142dc4c6c
eogus0512 <eogus0512@gmail.com>
2024-05-21 15:55:36 +0900

    feat: friend, user 예외코드 추가

---
+++ backend/src/main/java/momo/app/friend/exception/FriendErrorCode.java
@@ -0,0 +1,28 @@
+package momo.app.friend.exception;
+
+import lombok.Getter;
+import momo.app.common.error.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum FriendErrorCode implements ErrorCode {
+
+    FRIEND_REQUEST_ALREADY_EXISTS("3001", HttpStatus.CONFLICT,"이미 친구 요청 상태입니다."),
+    FRIEND_ALREADY_EXISTS("3002", HttpStatus.CONFLICT, "이미 친구입니다."),
+    FRIEND_NOT_FOUND("3003", HttpStatus.NOT_FOUND, "해당 친구가 존재하지 않습니다."),
+    FRIEND_DELETE_PERMISSION_DENIED("3004", HttpStatus.FORBIDDEN, "사용자와 친구 정보가 일치하지 않습니다.");
+
+    private final String code;
+    private final HttpStatus status;
+    private final String message;
+
+    FriendErrorCode(
+            String code,
+            HttpStatus status,
+            String message
+    ) {
+        this.code = code;
+        this.status = status;
+        this.message = message;
+    }
+}
