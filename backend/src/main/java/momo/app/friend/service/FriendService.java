ed485467661cc9005bc49acd31612e1d498019de
eogus0512 <eogus0512@gmail.com>
2024-05-21 16:21:28 +0900

    feat: 필요없는 코드 삭제

--- backend/src/main/java/momo/app/friend/service/FriendService.java
+++ backend/src/main/java/momo/app/friend/service/FriendService.java
@@ -16,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
