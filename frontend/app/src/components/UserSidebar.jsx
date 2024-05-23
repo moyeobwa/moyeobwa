350cc63ec85bd957c18209729b97abf22bdc0acb
eogus0512 <eogus0512@gmail.com>
2024-05-24 00:14:24 +0900

    feat: 그룹 만들기 onclick 설정

--- frontend/app/src/components/UserSidebar.jsx
+++ frontend/app/src/components/UserSidebar.jsx
@@ -1,16 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import Profile from './Profile';
 import MyGatheringList from './MyGatheringList';
 import Button from './Button';
 import './UserSidebar.css';
 
 const UserSidebar = () => {
+  const nav = useNavigate();
+
+  const onClickCreate = () => {
+      nav("/gathering/new", { replace: true });
+  }
+
   return (
     <div className="sidebar">
       <Profile />
       <MyGatheringList />
-      <Button text={"그룹 만들기"}/>
+      <Button text={"그룹 만들기"} onClick={onClickCreate}/>
     </div>
-  );
+  );  ``
 }
 
 export default UserSidebar;
