eed885e939325a09dc1882363b2727274fc04799
eogus0512 <eogus0512@gmail.com>
2024-05-24 00:13:35 +0900

    feat: 내모임 리스트 구현

--- frontend/app/src/components/MyGatheringList.jsx
+++ frontend/app/src/components/MyGatheringList.jsx
@@ -1,13 +1,33 @@
 import './MyGatheringList.css';
+import { useNavigate } from "react-router-dom";
+
+const myGathering = [
+  {
+    id:11,
+    title:"소울 휘트니스 헬스 모임"
+  },
+  {
+    id:12,
+    title:"스파르타 코딩 모임"
+  }
+]
 
 const MyGatheringList = () => {
+  const nav = useNavigate();
+
+  const goGatheringPage = (id) => {
+      nav(`/gathering/${id}`)
+  }
+
   return (
     <div className="gathering_list">
       <h3>내 모임</h3>
       <ul>
-        <li>소울 휘트니스 헬스 모임</li>
-        <li>스파르타 코딩 모임</li>
-        {/* 다른 모임 항목 추가 */}
+        {myGathering.map((gathering) => (
+            <li key={gathering.id} onClick={() => goGatheringPage(gathering.id)}>
+              {gathering.title}
+            </li>
+        ))}
       </ul>
     </div>
   );
