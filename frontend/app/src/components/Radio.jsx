53c6cc04a3ed5ca24450edd2b1693cfcefa7f347
eogus0512 <eogus0512@gmail.com>
2024-05-24 00:14:09 +0900

    feat: 모임원 순, 최근 활동 순 정렬 완료

--- frontend/app/src/components/Radio.jsx
+++ frontend/app/src/components/Radio.jsx
@@ -1,14 +1,27 @@
 import './Radio.css';
 
-const Radio = () => {
+const Radio = ({ sortType, onChangeSortType }) => {
+
   return (
     <div className="radio-inputs">
         <label className="radio">
-            <input type="radio" name="radio" checked={true}/>
-            <span className="name">모임원 순</span>
+          <input 
+            type="radio" 
+            name="radio" 
+            value="memberOrder" 
+            checked={sortType === "memberOrder"} 
+            onChange={onChangeSortType} 
+          />
+          <span className="name">모임원 순</span>
         </label>
         <label className="radio">
-            <input type="radio" name="radio"/>
+          <input 
+            type="radio" 
+            name="radio" 
+            value="recency" 
+            checked={sortType === "recency"} 
+            onChange={onChangeSortType} 
+            />
             <span className="name">최근 활동 순</span>
         </label>
     </div>
