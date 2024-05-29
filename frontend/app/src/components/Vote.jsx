c978f859ce36827b464a14dc889d72fd224cad9c
eogus0512 <eogus0512@gmail.com>
2024-05-30 02:50:16 +0900

    feat: vote 컴포넌트 추가 (#79)

---
+++ frontend/app/src/components/Vote.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './Vote.css';
+
+const Vote = () => {
+
+  return (
+    <div className="Vote">
+        Vote
+    </div>
+  );
+};
+
+export default Vote;
