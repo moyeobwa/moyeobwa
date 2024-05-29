b119a492d0188b4028af0f74d2e13e4dbf3462e2
eogus0512 <eogus0512@gmail.com>
2024-05-30 02:54:58 +0900

    feat: 색깔 라디오 컴포넌트 구현 (#78)

---
+++ frontend/app/src/components/Calendar/Components/ColorRadio.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ColorRadio.css'; 
+const ColorRadio = ({ color, handleColor, isTheme }) => {
+  return (
+    <div className={`box ${isTheme ? 'theme' : ''}`}>
+      <div
+        className={`pink color ${color === 'pink' ? 'selected' : ''} ${
+          isTheme ? 'themeColor' : ''
+        }`}
+        onClick={() => handleColor('pink')}
+      ></div>
+      <div
+        className={`yellow color ${color === 'yellow' ? 'selected' : ''} ${
+          isTheme ? 'themeColor' : ''
+        }`}
+        onClick={() => handleColor('yellow')}
+      ></div>
+      <div
+        className={`green color ${color === 'green' ? 'selected' : ''} ${
+          isTheme ? 'themeColor' : ''
+        }`}
+        onClick={() => handleColor('green')}
+      ></div>
+    </div>
+  );
+}
+
+export default ColorRadio;
