e77c84475d01a4b6cbeec0d7163d1da93044dbaa
eogus0512 <eogus0512@gmail.com>
2024-05-30 02:49:29 +0900

    refator: 모임 사이드바 디자인 변경 (#79)

--- frontend/app/src/components/Sidebar.jsx
+++ frontend/app/src/components/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './Sidebar.css';
+import profile from "./../assets/profile.png"
+import Button from "./Button"
+import { RiMessage2Line } from "react-icons/ri";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { LuVote } from "react-icons/lu";
 
-const Sidebar = () => {
+const Sidebar = ({ activeTab, setActiveTab }) => {
   return (
     <div className="sidebar">
       <div className="sidebar-header">
@@ -9,19 +14,38 @@ const Sidebar = () => {
       </div>
       <div className="sidebar-menu">
         <div className="sidebar-item">
+          <img src={profile} alt="프로필 이미지" />
           <span>방장_id</span>
         </div>
         <div className="sidebar-links">
-          <p>Messages</p>
-          <p>Calendar</p>
-          <p>Votes</p>
+          <p
+            className={activeTab === 'Messages' ? 'active' : ''}
+            onClick={() => setActiveTab('Messages')}
+          >
+            <RiMessage2Line/>
+            {" 대화하기"}
+          </p>
+          <p
+            className={activeTab === 'Calendar' ? 'active' : ''}
+            onClick={() => setActiveTab('Calendar')}
+          >
+            <FaRegCalendarAlt/>
+            {" 일정잡기"}
+          </p>
+          <p
+            className={activeTab === 'Votes' ? 'active' : ''}
+            onClick={() => setActiveTab('Votes')}
+          >
+            <LuVote/>
+            {" 투표하기"}
+          </p>
         </div>
         <div className="sidebar-participants">
           <h3>모임원</h3>
-          <div className="participant-item">user1</div>
-          <div className="participant-item">user2</div>
-          <div className="participant-add">+</div>
+          <div className="participant-item">문진수</div>
+          <div className="participant-item">윤대현</div>
         </div>
+        <Button text={"친구초대"}/>
       </div>
     </div>
   );
