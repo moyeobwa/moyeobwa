4a71201c0195c5739d677b3f24fa15e977d2a18c
eogus0512 <eogus0512@gmail.com>
2024-05-29 16:16:18 +0900

    feat: 로그인 response 생성

---
+++ frontend/app/src/pages/LoginResponse.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Cookies from 'universal-cookie';
+
+const LoginResponse = () => {
+    const [cookieValue, setCookieValue] = useState('');
+    const cookies = new Cookies();
+
+    useEffect(() => {
+        const token = cookies.get('Authorization-refresh');
+        if (token) {
+            setCookieValue(token);
+        }
+    }, [cookies]);
+
+    return (  
+        <div className="LoginResponse">
+            Token : {token}
+        </div>
+    );
+}
+ 
+export default LoginResponse;
