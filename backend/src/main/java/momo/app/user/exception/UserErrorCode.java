168191614ff3b64c3e5c61b20cbc99b5362577d4
eogus0512 <eogus0512@gmail.com>
2024-06-10 23:35:34 +0900

    feat: User errorcode 수정 (#87)

--- backend/src/main/java/momo/app/user/exception/UserErrorCode.java
+++ backend/src/main/java/momo/app/user/exception/UserErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum UserErrorCode implements ErrorCode {
-    User_NOT_FOUND("2001", HttpStatus.NOT_FOUND,"사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND("2001", HttpStatus.NOT_FOUND,"사용자를 찾을 수 없습니다.");
 
     private final String code;
     private final HttpStatus status;
