066ee8cdc4875c0f4cf245965389c17b3928107f
jinsu868 <mjinsu098@gmail.com>
2024-06-12 18:14:33 +0900

    fix: LoginResponse 페이지 삭제 (#86)

--- frontend/app/src/router.jsx
+++ frontend/app/src/router.jsx
@@ -7,7 +7,6 @@ const SignUp = lazy(() => import("./pages/SignUp"));
 const Login = lazy(() => import("./pages/Login"));
 const Gathering = lazy(() => import("./pages/Gathering"));
 const NewGathering = lazy(() => import("./pages/NewGathering"));
-const LoginResponse = lazy(() => import("./pages/LoginResponse"));
 
 const router = createBrowserRouter([
     {
@@ -23,16 +22,12 @@ const router = createBrowserRouter([
         element: <Suspense fallback={Loading}><Login /></Suspense>
     },
     {
-        path: "/gathering",
-        element: <Suspense fallback={Loading}><Gathering /></Suspense>
-    },
-    {
         path: "/gathering/new",
         element: <Suspense fallback={Loading}><NewGathering /></Suspense>
     },
     {
-        path: "/login/response",
-        element: <Suspense fallback={Loading}><LoginResponse /></Suspense>
+        path: "/gathering/:id", 
+        element: <Suspense fallback={Loading}><Gathering /></Suspense>
     }
 ]);
 
