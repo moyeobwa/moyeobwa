122982de2df1faa0b86f03b36e23781d0fe86c9b
eogus0512 <eogus0512@gmail.com>
2024-06-24 16:01:49 +0900

    feat: 일정 서비스 로직 구현 완료 및 예외 처리 (#103)

--- backend/src/main/java/momo/app/schedule/service/ScheduleService.java
+++ backend/src/main/java/momo/app/schedule/service/ScheduleService.java
@@ -10,12 +10,21 @@ import momo.app.schedule.domain.Schedule;
 import momo.app.schedule.domain.ScheduleRepository;
 import momo.app.schedule.dto.request.ScheduleCreateRequest;
 import momo.app.schedule.dto.request.ScheduleUpdateRequest;
+import momo.app.schedule.dto.response.ScheduleResponse;
 import momo.app.schedule.exception.ScheduleErrorCode;
 import momo.app.user.domain.User;
 import momo.app.user.domain.UserRepository;
 import momo.app.user.exception.UserErrorCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static momo.app.gathering.exception.GatheringErrorCode.GATHERING_NOT_FOUND;
+import static momo.app.gathering.exception.GatheringErrorCode.USER_NOT_IN_GATHERING;
+import static momo.app.schedule.exception.ScheduleErrorCode.SCHEDULE_NOT_FOUND;
+import static momo.app.user.exception.UserErrorCode.USER_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -25,9 +34,14 @@ public class ScheduleService {
     private final GatheringRepository gatheringRepository;
     private final ScheduleRepository scheduleRepository;
 
-    public Long create(Long id, ScheduleCreateRequest request, AuthUser authUser) {
+    public Long create(
+            ScheduleCreateRequest request,
+            AuthUser authUser
+    ) {
         User user = findUser(authUser);
-        Gathering gathering = findGathering(id);
+        Gathering gathering = findGathering(request.gatheringId());
+
+        validateUserInGathering(request.gatheringId(), user);
 
         Schedule schedule = Schedule.builder()
                 .color(request.color())
@@ -44,23 +58,56 @@ public class ScheduleService {
         return schedule.getId();
     }
 
-    public void update(Long id, ScheduleUpdateRequest request) {
+    public void update(
+            Long id,
+            ScheduleUpdateRequest request,
+            AuthUser authUser
+    ) {
+        User user = findUser(authUser);
         Schedule schedule = findSchedule(id);
+
+        schedule.validateUserAndSchedule(user);
+
         schedule.update(request);
+    }
+
+    public List<ScheduleResponse> get(Long gatheringId, LocalDate date) {
+        Gathering gathering = findGathering(gatheringId);
+        List<Schedule> schedules = scheduleRepository.findAllByGatheringIdAndDate(gathering, date);
+
+        return schedules.stream()
+                .map(schedule -> ScheduleResponse.from(schedule))
+                .toList();
+
+    }
+
+    public void delete(Long id, AuthUser authUser) {
+        User user = findUser(authUser);
+        Schedule schedule = findSchedule(id);
+
+        schedule.validateUserAndSchedule(user);
+
+        scheduleRepository.delete(schedule);
     }
 
     private User findUser(AuthUser authUser) {
         return userRepository.findById(authUser.getId())
-                .orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
+                .orElseThrow(() -> new BusinessException(USER_NOT_FOUND));
     }
 
     private Gathering findGathering(Long id) {
         return gatheringRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(GatheringErrorCode.GATHERING_NOT_FOUND));
+                .orElseThrow(() -> new BusinessException(GATHERING_NOT_FOUND));
     }
 
     private Schedule findSchedule(Long id) {
         return scheduleRepository.findById(id)
-                .orElseThrow(() -> new BusinessException(ScheduleErrorCode.SCHEDULE_NOT_FOUND));
+                .orElseThrow(() -> new BusinessException(SCHEDULE_NOT_FOUND));
+    }
+
+    private void validateUserInGathering(Long gatheringId, User user) {
+        if (!gatheringRepository.checkExistsByGatheringIdAndUserId(gatheringId, user.getId())) {
+            throw new BusinessException(USER_NOT_IN_GATHERING);
+        }
     }
 }
