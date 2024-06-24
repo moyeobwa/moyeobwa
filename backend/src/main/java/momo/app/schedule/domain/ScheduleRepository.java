1a004c707839bd24ddeb95fad4dbf366c1d3e143
eogus0512 <eogus0512@gmail.com>
2024-06-24 16:01:18 +0900

    feat: get 쿼리 문 수정 (#103)

--- backend/src/main/java/momo/app/schedule/domain/ScheduleRepository.java
+++ backend/src/main/java/momo/app/schedule/domain/ScheduleRepository.java
@@ -1,7 +1,15 @@
 package momo.app.schedule.domain;
 
+import momo.app.gathering.domain.Gathering;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
+    @Query("SELECT s FROM Schedule s WHERE s.gathering = :gathering AND s.date = :date")
+    List<Schedule> findAllByGatheringIdAndDate(Gathering gathering, LocalDate date);
 }
