23a2558eeffc91a6e61aed7e0bfa7924877d9e2c
eogus0512 <eogus0512@gmail.com>
2024-06-24 16:00:39 +0900

    feat: dto 추가 및 수정 (#103)

--- backend/src/main/java/momo/app/schedule/dto/request/ScheduleCreateRequest.java
+++ backend/src/main/java/momo/app/schedule/dto/request/ScheduleCreateRequest.java
@@ -10,6 +10,7 @@ public record ScheduleCreateRequest(
         String title,
         String content,
         LocalDate date,
-        LocalTime time
+        LocalTime time,
+        Long gatheringId
 ) {
 }
