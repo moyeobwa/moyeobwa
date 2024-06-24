01d43ac2f66b55c592d518a1c5b8f350d6beaa0b
eogus0512 <eogus0512@gmail.com>
2024-06-24 16:00:59 +0900

    feat: 일정 컨트롤러 구현 (#103)

---
+++ backend/src/main/java/momo/app/schedule/controller/ScheduleController.java
@@ -0,0 +1,63 @@
+package momo.app.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import momo.app.auth.dto.AuthUser;
+import momo.app.schedule.dto.request.ScheduleCreateRequest;
+import momo.app.schedule.dto.request.ScheduleUpdateRequest;
+import momo.app.schedule.dto.response.ScheduleResponse;
+import momo.app.schedule.service.ScheduleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/schedules")
+public class ScheduleController {
+    private final ScheduleService scheduleService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(
+            @RequestBody ScheduleCreateRequest request,
+            @AuthenticationPrincipal AuthUser authUser
+    ) {
+        Long scheduleId = scheduleService.create(request, authUser);
+
+        return ResponseEntity.created(URI.create("/api/v1/schedules/" + scheduleId))
+                .build();
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> update(
+            @PathVariable Long id,
+            @RequestBody ScheduleUpdateRequest request,
+            @AuthenticationPrincipal AuthUser authUser
+    ) {
+        scheduleService.update(id, request, authUser);
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<ScheduleResponse>> get(
+            @PathVariable Long id,
+            @RequestParam LocalDate date
+    ) {
+        return ResponseEntity.ok(scheduleService.get(id, date));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long id,
+            @AuthenticationPrincipal AuthUser authUser
+    ) {
+        scheduleService.delete(id, authUser);
+        return ResponseEntity.ok()
+                .build();
+    }
+
+}
