4d13f80754660f9958b38bd4a8a03d9f6d9ebe6b
eogus0512 <eogus0512@gmail.com>
2024-06-24 16:00:05 +0900

    feat: 에러 추가 (#103)

--- backend/src/main/java/momo/app/schedule/exception/ScheduleErrorCode.java
+++ backend/src/main/java/momo/app/schedule/exception/ScheduleErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ScheduleErrorCode implements ErrorCode {
-    SCHEDULE_NOT_FOUND("7001", HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다.");
+    SCHEDULE_NOT_FOUND("7001", HttpStatus.NOT_FOUND, "일정을 찾을 수 없습니다."),
+    SCHEDULE_NOT_EQUAL_USER("7002", HttpStatus.BAD_REQUEST, "일정과 사용자 정보가 일치하지 않습니다.");
 
     private final String code;
     private final HttpStatus status;
