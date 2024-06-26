ca1b6ae0340f8fff987e431b296ab5bddfc66a7b
jinsu868 <mjinsu098@gmail.com>
2024-06-26 18:40:38 +0900

    feat: 모임 조회 정렬기준, 카테고리 설정 (#99)

--- backend/src/main/java/momo/app/gathering/domain/GatheringSortType.java
+++ backend/src/main/java/momo/app/gathering/domain/GatheringSortType.java
@@ -2,14 +2,13 @@ package momo.app.gathering.domain;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public enum Category {
-    EXERCISE("EXERCISE", "운동"),
-    STUDY("STUDY", "공부"),
-    GAME("GAME", "게임"),
-    FOOD("FOOD", "음식"),
-    TRAVEL("TRAVEL", "여행");
+public enum GatheringSortType {
+    LATEST("LATEST", "최신순"),
+    MEMBER_COUNT("MEMBER_COUNT", "모임원순");
 
     private String key;
     private String value;
