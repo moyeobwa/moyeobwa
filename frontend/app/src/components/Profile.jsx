0221f5e856c7b3f92a57c188d2ac61c2a6b70462
eogus0512 <eogus0512@gmail.com>
2024-06-30 13:43:12 +0900

    feat: 프로필 기능 API 구현 (#113)

--- frontend/app/src/components/Profile.jsx
+++ frontend/app/src/components/Profile.jsx
@@ -1,13 +1,43 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import './Profile.css';
-import profile from "./../assets/profile.png"
+import defaultProfile from "./../assets/profile.png"
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+  const token = localStorage.getItem('token');
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/api/v1/users`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        if (response.status === 200) {
+          setUser(response.data);
+        }
+      } catch (error) {
+        console.error(error);
+        alert('네트워크 오류가 발생했습니다.');
+      }
+    };
+
+    fetchUser();
+  }, [token]);
+
   return (
     <div className="profile">
-      <img src={profile} alt="프로필 이미지" />
+      <img src={user?.imageUrl || defaultProfile} alt="프로필 이미지" />
       <div className="info">
-        <div className="nickname">윤대현</div>
-        <div className="email">eogus0512@gmail.com</div>
+        <div className="names">
+          <div className="name">{user?.name || '이름'}</div>
+          <div className="nickname">{user?.nickname || '닉네임'}</div>
+        </div>
+        <div className="email">{user?.email || '이메일'}</div>
       </div>
     </div>
   );
