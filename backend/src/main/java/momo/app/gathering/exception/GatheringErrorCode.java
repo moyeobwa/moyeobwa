938d7b1353b5cdb696f9eef3576742fe03db7459
eogus0512 <eogus0512@gmail.com>
2024-07-07 21:50:31 +0900

    feat: 친구 초대 관련 에러코드 추가 (#121)

--- backend/src/main/java/momo/app/gathering/exception/GatheringErrorCode.java
+++ backend/src/main/java/momo/app/gathering/exception/GatheringErrorCode.java
@@ -11,7 +11,10 @@ import org.springframework.http.HttpStatus;
 public enum GatheringErrorCode implements ErrorCode {
     GATHERING_NOT_FOUND("6001", HttpStatus.BAD_REQUEST, "모임방이 존재하지 않습니다."),
     NOT_MANAGER("6002", HttpStatus.FORBIDDEN, "모임방의 메니저가 아닙니다."),
-    USER_NOT_IN_GATHERING("6003", HttpStatus.BAD_REQUEST, "모임방에 존재하지 않는 유저입니다.");
+    USER_NOT_IN_GATHERING("6003", HttpStatus.BAD_REQUEST, "모임방에 존재하지 않는 유저입니다."),
+    NOT_TO_USER("6004", HttpStatus.FORBIDDEN, "사용자 정보가 일치하지 않습니다."),
+    GATHERING_INVITE_NOT_FOUND("6005", HttpStatus.BAD_REQUEST, "모임 초대 정보가 존재하지 않습니다."),
+    GATHERING_INVITE_ALREADY_EXISTS("6006", HttpStatus.CONFLICT, "이미 초대하였습니다.");
 
     private String code;
     private HttpStatus statue;
